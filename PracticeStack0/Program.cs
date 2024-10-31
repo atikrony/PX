35d7ebe780fd3e938fc3871f964910ad841c0084
atikrony <atik.rony2013@gmail.com>
2024-10-31 23:03:51 +0600

    Stack

---
+++ PracticeStack0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace PracticeStack0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+
+            Console.WriteLine("Inserted numbers:");
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("poped number:");
+
+           
+            numbers.Pop();
+               
+           foreach (var number in numbers)
+            {
+               Console.WriteLine(number);
+            }
+
+        }
+    }
+}
